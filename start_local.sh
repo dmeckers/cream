c6fbc9303987dacbd84a23bfe568873153fe12c1
dmeckers <morph232@inbox.lv>
2024-08-09 19:39:09 +0300

    Add submodule for cream-api repository

---
+++ start_local.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Prompt for Docker login
+docker login
+
+# Check if the login was successful
+if [ $? -eq 0 ]; then
+  echo "Login successful. Starting Docker Compose..."
+  docker-compose -f docker-compose.dev.yml up -d --build
+else
+  echo "Login failed. Please check your credentials."
+  exit 1
+fi
