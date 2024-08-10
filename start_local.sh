4031d71a847cc820cc62c8e14ea75c4f0fdbb3c0
dmeckers <morph232@inbox.lv>
2024-08-11 02:38:15 +0300

    chore: Add .gitignore file and submodules for cream-tg and cream-db

--- start_local.sh
+++ start_local.sh
@@ -6,7 +6,7 @@ docker login
 # Check if the login was successful
 if [ $? -eq 0 ]; then
   echo "Login successful. Starting Docker Compose..."
-  docker-compose -f docker-compose.dev.yml up -d --build
+  docker-compose -f docker-compose.yml up -d --build
 else
   echo "Login failed. Please check your credentials."
   exit 1
